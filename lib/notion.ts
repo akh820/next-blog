0069a662eff983922bed59dc02ec9b491d353839
akh820 <akh8202@gmail.com>
2025-09-30 19:02:58 +0900

    feat : 정렬 기능 추가로 인한 Props 전달 수정

--- lib/notion.ts
+++ lib/notion.ts
@@ -90,7 +90,7 @@ export const getPostBySlug = async (
   // return getPageMetadata(response);
 };
 
-export const getPublishedPosts = async (tag?: string): Promise<Post[]> => {
+export const getPublishedPosts = async (tag?: string, sort?: string): Promise<Post[]> => {
   const response = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
     filter: {
@@ -116,7 +116,7 @@ export const getPublishedPosts = async (tag?: string): Promise<Post[]> => {
     sorts: [
       {
         property: 'Date',
-        direction: 'descending',
+        direction: sort === 'latest' ? 'descending' : 'ascending',
       },
     ],
   });
