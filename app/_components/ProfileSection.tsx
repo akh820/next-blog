7b064a1c081fcfc672958bf8bd1f42b17a3fe100
akh820 <akh8202@gmail.com>
2025-10-16 17:30:58 +0900

    feat : 프로필 컴포넌트 분리 및 임시 다크 모드 프로필 사진 등록

--- app/_components/ProfileSection.tsx
+++ app/_components/ProfileSection.tsx
@@ -1,3 +1,4 @@
+import { ProfileImage } from '@/components/ProfileImage';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Youtube, Github, BookOpen, Instagram } from 'lucide-react';
@@ -30,13 +31,7 @@ export default function ProfileSection() {
           <div className="jusity-center flex">
             <div className="bg-muted rounded-full p-2">
               <div className="h-36 w-36 overflow-hidden rounded-full">
-                <Image
-                  src="/images/favicon.PNG"
-                  alt="Hong"
-                  width={144}
-                  height={144}
-                  className="object-cover"
-                />
+                <ProfileImage />
               </div>
             </div>
           </div>
